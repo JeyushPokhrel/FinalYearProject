d9dc36bafffedde418a266161d6258a3dcf413e8
Jeyush Pokhrel <jeyushpokhrel@gmail.com>
2026-05-02 23:58:02 +0545

    production fix

--- rawproject/backend/index.js
+++ rawproject/backend/index.js
@@ -4,9 +4,9 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import chatRoutes from "./routes/chatRoutes.js";
+
+// Production Config v1.0.1
 dotenv.config();
-
-
 
 const app = express()
 app.use(express.json())
@@ -15,7 +15,8 @@ app.use(cors({
     origin:[
       "http://localhost:5173",
       "http://localhost:5174",
-    ], // Adjust this to your frontend URL
+      "https://finalyearproject-legal-ai-frontend.onrender.com",
+    ],
     credentials: true
 }))
 
@@ -24,10 +25,7 @@ connectDB();
 app.use('/api/auth', authRoutes);
 app.use("/api/chat", chatRoutes);
 
-
-
 const PORT = process.env.PORT || 5000
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
