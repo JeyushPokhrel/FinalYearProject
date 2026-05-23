1172938cf06bf0fefdf5e1fe055edab331c1c3c2
Jeyush Pokhrel <jeyushpokhrel@gmail.com>
2026-05-23 22:28:14 +0545

    apifix

--- rawproject/frontend/src/api.js
+++ rawproject/frontend/src/api.js
@@ -1,5 +1,5 @@
- const API_BASE_URL = "http://localhost:5000/api"; // local
-//  const API_BASE_URL = "https://finalyearproject-ian2.onrender.com/api"; // Production
+//  const API_BASE_URL = "http://localhost:5000/api"; // local
+ const API_BASE_URL = "https://finalyearproject-ian2.onrender.com/api"; // Production
 
 export default API_BASE_URL;
 
